33f8a759a1b94d9621cbe49f073ad18eabd4ad6b
Chubbray <chubbray1@gmail.com>
2020-10-17 17:00:32 -0600

    working on the front end

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql'
+});
+
 function App() {
   return (
     <Router>
-      <>
+      <ApolloProvider client={client}>
         <Navbar />
         <Switch>
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
-      </>
+      </ApolloProvider>
     </Router>
   );
 }
